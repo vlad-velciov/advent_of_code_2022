33870c446671cf57b7ccaed801af4048644105e6
Vlad Velciov <velciov.vlad@gmail.com>
2022-12-05 12:07:28 +0200

    fixes test

--- spec/day5/instruction_spec.rb
+++ spec/day5/instruction_spec.rb
@@ -2,7 +2,7 @@ describe Day5::Instruction do
 
   describe '#initialize' do
     it 'creates an instruction from a line' do
-      expect(Day5::Instruction.new('move 10 from 2 to 3').to_s).to eq('10-2-3')
+      expect(Day5::Instruction.new('move 10 from 2 to 3').to_s).to eq('10-1-2')
     end
   end
 
