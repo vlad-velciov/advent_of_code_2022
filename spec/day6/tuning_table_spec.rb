0ea5e678c0a2fb956431dc9190267a6b264ffa50
Vlad Velciov <velciov.vlad@gmail.com>
2022-12-06 13:30:50 +0200

    Ability to run both parts

--- spec/day6/tuning_table_spec.rb
+++ spec/day6/tuning_table_spec.rb
@@ -13,4 +13,14 @@ describe Day6::TuningTable do
   it 'runs the input' do
     expect(Day6::TuningTable.new.find).to eq(1623)
   end
+
+  describe 'part two' do
+    it 'runs for part two' do
+      expect(Day6::TuningTable.new(marker_size: 14).find).to eq(3774)
+    end
+
+    it 'runs the example 1' do
+      expect(Day6::TuningTable.new(marker_size: 14, file_path: "#{__dir__}/example_1.txt").find).to eq(19)
+    end
+  end
 end
--- src/day6/tuning_table.rb
+++ src/day6/tuning_table.rb
@@ -1,11 +1,10 @@
 module Day6
   class TuningTable
-    MARKER_SIZE = 4
-
-    def initialize(file_path: "#{__dir__}/input.txt")
+    def initialize(file_path: "#{__dir__}/input.txt", marker_size: 4)
       @file_path = file_path
       @marker_hash = {}
       @marker_index = 0
+      @marker_size = marker_size
     end
 
     def find
@@ -13,27 +12,30 @@ module Day6
         line.chars.each_with_index do |char, index|
           push_to_marker(char, index)
 
-          return marker_index+4 if marker?
+          return marker_hash.min_by{|k,v| v}.last + marker_size if marker?
         end
       end
+
+      0
     end
 
     private
 
     def marker?
-      marker_hash.size == MARKER_SIZE
+      marker_hash.compact.size == marker_size
     end
 
     def push_to_marker(char, index)
-      if marker_hash[char]
-        @marker_hash = {}
+      unless marker_hash[char].nil?
+        found_index = marker_hash[char]
+        @marker_hash.each{ |key, current_index| @marker_hash.delete(key) if current_index <= found_index}
         @marker_index = index
       end
 
-      @marker_hash[char] = true
+      @marker_hash[char] = index
     end
 
-    attr_accessor :file_path, :marker_hash, :marker_index
+    attr_accessor :file_path, :marker_hash, :marker_index, :marker_size
     def read_file
       file = File.open(file_path)
       file.readlines.each do |line|
